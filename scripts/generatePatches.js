17e3740554c21c6926cc686f6667ab6b8fed29d8
yut <yuth_style88@hotmail.com>
2025-04-28 03:29:39 +0700

    update

---
+++ scripts/generatePatches.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const config = require('../src/brave/build/commands/lib/config');
+const updatePatches  = require('../src/brave/build/commands/lib/updatePatches');
+
+const braveDir = path.join(config.srcDir, 'brave');
+const patchesDir = path.join(config.srcDir, 'ibrowe', 'src', 'patches');
+
+Promise.all([
+    updatePatches(braveDir, patchesDir)
+])
+    .then(() => {
+        console.log('Done.');
+    })
+    .catch(err => {
+        console.error('Error updating patch files:');
+        console.error(err);
+    });
